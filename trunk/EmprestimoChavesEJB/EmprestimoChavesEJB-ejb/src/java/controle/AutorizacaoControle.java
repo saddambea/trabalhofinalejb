8ffb5a754f0718b069d1032745811f174ca56038
alebae@gmail.com <alebae@gmail.com@f6e31d6e-bd81-7158-8892-08429ba84fc1>
2013-12-07 13:52:32 +0000

    Commit da classe do exercício 4.

--- trunk/EmprestimoChavesEJB/EmprestimoChavesEJB-ejb/src/java/controle/AutorizacaoControle.java
+++ trunk/EmprestimoChavesEJB/EmprestimoChavesEJB-ejb/src/java/controle/AutorizacaoControle.java
@@ -5,6 +5,12 @@
 package controle;
 
 import dao.AutorizacaoDAO;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.util.Arrays;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -64,6 +70,110 @@ public class AutorizacaoControle{
         } catch (Exception e) {
         }
         return true;      
+        
+    }
+    
+    public boolean getAssinaRSA() throws Exception{
+        //define as variáveis
+        KeyPairGenerator chaves = null;
+        Signature sgn = null;
+        Signature vrf = null;
+        byte [] assina = null;
+        
+        try{
+            //inicializa e gera as chaves
+            chaves = KeyPairGenerator.getInstance("RSA");
+            chaves.initialize(1024);
+            System.out.println("Gerando o par de chaves...");
+            KeyPair par = chaves.generateKeyPair(); //gera as chaves        
+            PrivateKey priv = par.getPrivate();   //obtem chave privada
+            PublicKey pub = par.getPublic();  //obtem chave pública
+
+            //cria uma instância do RSA com MD5
+            sgn = Signature.getInstance("MD5withRSA");
+
+            //inicializa o algoritmo de assinatura
+            sgn.initSign(priv); 
+
+            //adiciona a mensagem ao algoritmo
+            String mensagem = "abc";
+            sgn.update(mensagem.getBytes());
+
+            //faz a assinatura
+            assina = sgn.sign();
+            
+            //*** inicia a verificação ***
+
+            // cria uma instância do RSA com MD5
+            vrf = Signature.getInstance("MD5withRSA");
+
+            //inicializa uma verificação com a chave pública do parâmetro
+            vrf.initVerify(pub);
+
+            //adiciona o texto ao algoritmo
+            vrf.update(mensagem.getBytes());
+
+            //faz a verificação com a assinatura do parâmetro
+            boolean ok = vrf.verify(assina);
+            return ok;
+        }catch(Exception e){
+            throw new Exception("Erro! Não faça isso");
+        }
+        
+    }
+    
+    public boolean getAssinaDSA() throws Exception{
+        //define as variáveis
+        KeyPairGenerator chaves = null;
+        Signature sgn = null;
+        Signature vrf = null;
+        byte [] assina = null;
+        
+        try{
+            //define uma mensagem
+            String mensagem = "teste teste";
+
+            //cria uma instância do gerador de chaves 
+            chaves = KeyPairGenerator.getInstance("DSA");
+
+            //cria uma instância do DSA
+            sgn = Signature.getInstance("DSA");
+
+            //inicializa e gera as chaves
+            chaves.initialize(512);
+            System.out.println("Gerando o par de chaves...");
+            KeyPair par = chaves.generateKeyPair(); //gera as chaves        
+            PrivateKey priv = par.getPrivate();   //obtem chave privada
+            PublicKey pub = par.getPublic();  //obtem chave pública
+
+            sgn.initSign(priv); //inicializa o algoritmo de assinatura
+
+            //adiciona a mensagem ao algoritmo
+            sgn.update(mensagem.getBytes());
+
+            //faz a assinatura
+            assina = sgn.sign();
+            
+            boolean ok;
+
+            //inicia a verificação
+            // cria uma instância do DAS
+            vrf = Signature.getInstance("DSA");
+
+            //inicializa uma verificação com a chave pública do parâmetro
+            vrf.initVerify(pub);
+
+            //adiciona o texto ao algoritmo
+            vrf.update(mensagem.getBytes());
+
+            //faz a verificação com a assinatura do parâmetro
+            ok = vrf.verify(assina);
+
+            //saída da parte de verificação
+            return ok;
+        }catch(Exception e){
+            throw new Exception("Erro! Não Faça isso!");
+        }
         
     }
 
