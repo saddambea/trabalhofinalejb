ce6b401346eb1698abee889d754d4663ed29e16d
daniellenzi <daniellenzi@f6e31d6e-bd81-7158-8892-08429ba84fc1>
2013-12-07 13:35:27 +0000

    Inserido a criptografia e a descriptografia da chave RSA (Exercício 03)

--- EmprestimoChavesEJB/EmprestimoChavesEJB-ejb/src/java/criptografia/Criptografia.java
+++ EmprestimoChavesEJB/EmprestimoChavesEJB-ejb/src/java/criptografia/Criptografia.java
@@ -97,4 +97,67 @@ public class Criptografia {
     }
     
     
+    public byte[] getCriptografiaRSA(String texto) throws Exception{
+    
+         // Gera um par de chaves RSA de 1024 bits  
+         KeyPairGenerator gerador = KeyPairGenerator.getInstance("RSA");  
+         gerador.initialize(1024);  
+         KeyPair chaves = gerador.generateKeyPair();  
+
+         // Cria uma implementação do RSA  
+         Cipher cifra = Cipher.getInstance("RSA");  
+
+         //Inicializa o algoritmo para criptografiar a mensagem com a chave pública  
+         cifra.init(Cipher.ENCRYPT_MODE,chaves.getPublic());  
+
+         // Criptgrafa o texto inteiro  
+         byte[] mensagemCifrada = cifra.doFinal(texto.getBytes());  
+
+         // Inicializa o algoritmo para decriptografar com a chave privada
+         cifra.init(Cipher.DECRYPT_MODE,chaves.getPrivate());  
+         
+         // Decifra a mensagem
+         byte[] mensagemOriginal = cifra.doFinal(mensagemCifrada);  
+
+         //saídas do exemplo        
+         //System.out.println("Chave Publica: "+chaves.getPublic().toString());  
+         //System.out.println("Chave Privada: "+chaves.getPrivate().toString());  
+
+         //System.out.print("texto cifrado (string): " + new String(mensagemCifrada));  
+         //System.out.print("texto decifrado (string): " + new String(mensagemOriginal));       
+         return mensagemCifrada;
+    }
+    
+    public byte[] getDescriptografiaRSA(byte[] cripto) throws Exception{
+    
+         // Gera um par de chaves RSA de 1024 bits  
+         KeyPairGenerator gerador = KeyPairGenerator.getInstance("RSA");  
+         gerador.initialize(1024);  
+         KeyPair chaves = gerador.generateKeyPair();  
+
+         // Cria uma implementação do RSA  
+         Cipher cifra = Cipher.getInstance("RSA");  
+
+         //Inicializa o algoritmo para criptografiar a mensagem com a chave pública  
+         //cifra.init(Cipher.ENCRYPT_MODE,chaves.getPublic());  
+
+         // Criptgrafa o texto inteiro  
+         //byte[] mensagemCifrada = cifra.doFinal(texto.getBytes());  
+
+         // Inicializa o algoritmo para decriptografar com a chave privada
+         cifra.init(Cipher.DECRYPT_MODE,chaves.getPrivate());  
+         
+         // Decifra a mensagem
+         byte[] mensagemOriginal = cifra.doFinal(cripto);  
+
+         //saídas do exemplo        
+         //System.out.println("Chave Publica: "+chaves.getPublic().toString());  
+         //System.out.println("Chave Privada: "+chaves.getPrivate().toString());  
+
+         //System.out.print("texto cifrado (string): " + new String(mensagemCifrada));  
+         //System.out.print("texto decifrado (string): " + new String(mensagemOriginal));       
+         return mensagemOriginal;
+    }
+    
+    
 }
